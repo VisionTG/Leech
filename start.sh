08d42593cd33ccfe4a122d4c0a9e6675727f57e1
Vision <105115761+VisionTG@users.noreply.github.com>
2022-08-07 14:47:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,4 @@
-#bin #!bash
-# -*- coding: utf-8 -*-
-# Shrimadhav U K | gautamajay52 | MaxxRider | 5MysterySD | Other Contributors 
-#
-# Copyright 2022 - TeamTele-LeechX
-# 
-# This is Part of < https://github.com/5MysterySD/Tele-LeechX >
-# All Right Reserved
 
-## Adding Files ++++
-ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
-## Adding Files ----
 
 if [[ -n $CONFIG_ENV_URL ]]; then
   echo " Found config.env File 📁📁"
@@ -19,6 +8,6 @@ fi
 if [ -f .env ] ; then  set -o allexport; source .env; set +o allexport ; fi
 
 
-echo "Starting Your Tele-LeechX... ♻️♻️"
+echo "Starting Bot...😺😺."
 python3 update.py && python3 -m tobrot
 
